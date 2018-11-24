192f98c161e1cf9470914ee6cf882bba6986dc8f
Nathan <nathanarohde@gmail.com>
2018-11-24 14:17:13 -0800

    Added font for body text.  Moved text out of white background area.

--- src/Components/Cartoon/Cartoon.js
+++ src/Components/Cartoon/Cartoon.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Cartoon.css'
 
 let cartoon = ( props ) => {
   let source= 'https://github.com/nathanarohde/vfd_home_site/blob/master/src/Cartoons/' + props.source + '/Panels/1.jpg?raw=true'
 
   return (
-    <img src={ source } alt="Hillarious Cartoon"/>
+    <div className="cartoonBox">
+      <img src={ source } alt="Hillarious Cartoon"/>
+    </div>
   );
 }
 
