403cc75cf17261c809b0b6b71ce59678abe4812f
Nathan <nathanarohde@gmail.com>
2018-11-28 18:36:58 -0800

    Refactoring of loading cartoons on scroll.

--- src/Containers/Display/Display.js
+++ src/Containers/Display/Display.js
@@ -12,10 +12,9 @@ import * as actions from '../../Store/actions';
 class Display extends Component {
   // scrollCounter set here because scroll event listener set in mount cannot access Redux state updates
   state = {
-    scrollCounter: 1,
     cartoonData: '',
     displayedCartoons: [ ],
-    height: 0
+    height: 0,
   }
 
   componentDidMount() {
@@ -25,10 +24,9 @@ class Display extends Component {
     // Wrong cartoon will load if startingPage is out of parameters in URL bar
       //  - This is fixed in update because Redux not available on Mount
     if ( this.props.startingPage > 0 ) {
-      this.props.onSetCurrentDisplayedCartoon(
-        // Starting page is a string and therefore must be parsed.
-        parseInt(this.props.startingPage)
-      );
+      this.setDisplayedCartoons( this.props.startingPage );
+      this.props.onSetCurrentDisplayedCartoon( parseInt(this.props.startingPage) );
+      // Starting page is a string and therefore must be parsed.
     }
 
   }
@@ -39,28 +37,32 @@ class Display extends Component {
 
   handleScroll = ( event ) => {
     // this.props.currentDisplayedCartoon does not update because it is initialized on mount and cannot access Redux updates
-    if ( ( this.props.currentDisplayedCartoon + this.state.scrollCounter - 1 ) < this.props.lastCartoon &&
+    if ( this.props.currentDisplayedCartoon < this.props.lastCartoon &&
       Math.floor(document.documentElement.scrollHeight - document.documentElement.scrollTop) === Math.ceil(document.documentElement.clientHeight)
     ){
-      this.props.onSetCurrentDisplayedCartoon( this.props.currentDisplayedCartoon + this.state.scrollCounter )
-      this.setDisplayedCartoons( this.props.currentDisplayedCartoon + this.state.scrollCounter )
-      this.setState({scrollCounter: this.state.scrollCounter + 1 })
+      this.setDisplayedCartoons( this.props.currentDisplayedCartoon + 1 )
+      this.props.onSetCurrentDisplayedCartoon( this.props.currentDisplayedCartoon + 1 )
     }
   }
 
   componentDidUpdate( prevProps ) {
     // Redux not available until component did update
     // In these three if scenarios there is no applicable cartoon so it is set to lastCartoon
-    if ( this.props.startingPage === undefined ||
+    if ( this.props.currentDisplayedCartoon === 0 ){
+      if (
+        this.props.startingPage === undefined ||
         this.props.startingPage === 0 ||
         this.props.startingPage > this.props.lastCartoon ){
           this.props.onSetCurrentDisplayedCartoon(
             this.props.lastCartoon
           );
-    } else {
-      this.props.onSetCurrentDisplayedCartoon(
-        parseInt(this.props.startingPage)
-      );
+          this.setDisplayedCartoons( this.props.lastCartoon );
+      } else {
+        this.props.onSetCurrentDisplayedCartoon(
+          parseInt( this.props.startingPage )
+        );
+        this.setDisplayedCartoons( this.props.startingPage );
+      }
     }
 
     // Eliminates calls which will fail.
@@ -68,7 +70,6 @@ class Display extends Component {
         this.props.currentDisplayedCartoon !== 0 &&
         this.props.currentDisplayedCartoon <= this.props.lastCartoon ){
       this.setDisplayedCartoons( this.props.currentDisplayedCartoon );
-      this.asyncGetCartoonData();
     }
 
   }
@@ -79,6 +80,7 @@ class Display extends Component {
         displayedCartoons: [...prevState.displayedCartoons, cartoon]
       }))
     }
+    // this.asyncGetCartoonData();
   };
 
   asyncGetCartoonData = () => {
@@ -103,13 +105,12 @@ class Display extends Component {
   render () {
     let cartoons = <p>Site is loading.</p>
 
-    if ( this.props.lastCartoon > 0 ){
-      cartoons = this.state.displayedCartoons.map(cartoon => {
-        return (
-          <Cartoon key={ cartoon } source={ cartoon } />
-        )
-      })
-    }
+    // console.log(this.props.currentDisplayedCartoon);
+    cartoons = this.state.displayedCartoons.map(cartoon => {
+      return (
+        <Cartoon key={ cartoon } source={ cartoon } />
+      )
+    })
       // <TitleBox date={ this.state.cartoonData.date } title={ this.state.cartoonData.title }/>
     return (
       <div id="displayField">
